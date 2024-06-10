800ec8b714140761cdd485a013e8fa1046a8f1f8
wangyihe <heheyoumingwyh@qq.com>
2024-06-10 13:33:02 +0800

    new

---
+++ build_x64.bat
@@ -0,0 +1,16 @@
+@echo off
+@REM conan install . --output-folder=build --build=missing --profile=conan_profile_x64
+
+mkdir build
+
+cd build
+
+cmake .. -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
+@REM -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Toolchain_FILE="conan_paths.cmake"
+
+cmake --build . --config DEBUG
+
+cd ../out/build/X86_64/
+
+echo ============Running MyProject.exe=============
+.\MyProject
