212dd1fe268d873d6b1e6517dea146d92ef179a8
wangyihe <heheyoumingwyh@qq.com>
2024-07-25 16:09:39 +0800

    chore: Refactor build_x64.bat script to change directory to ../out instead of ../out/build/X86_64/

--- build_x64.bat
+++ build_x64.bat
@@ -1,12 +1,21 @@
 @echo off
+
+REM 安装依赖项
 conan install . --build=missing --profile=conan_profile_x64
 
+REM 创建构建目录
 mkdir build
 
 cd build
 
-cmake .. -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Toolchain_FILE="conan_paths.cmake"
+REM 配置 CMake 项目
+cmake .. -G "Ninja" ^
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_TOOLCHAIN_FILE="%CD%\Release\generators\conan_toolchain.cmake" ^
+    -DCMAKE_PREFIX_PATH="%CD%\Release\generators"
 
+REM 构建项目
 cmake --build . --config DEBUG
 
 cd ../out
